10c3c92c72f9d1406150229e7d1b442eff7d71fb
manoelsouzadev <manoel.souza280@gmail.com>
2021-12-12 20:53:52 -0300

    Lendo arquivo .CSV e adicionando no banco de dados

---
+++ src/routes.ts
@@ -0,0 +1,60 @@
+import { Request, Response, Router } from "express";
+import { Readable } from "stream";
+import readLine from "readline";
+
+import multer from "multer";
+import { client } from "./database/client";
+
+const multerConfig = multer();
+
+const router = Router();
+
+interface Product{
+    code_bar: string;
+    description: string;
+    price: number;
+    quantity: number;
+}
+
+router.post("/products", multerConfig.single("file"), async (request: Request, response: Response) => {
+
+    const { file } = request;
+    const  buffer  = file?.buffer;
+
+    const readableFile = new Readable();
+    readableFile.push(buffer);
+    readableFile.push(null);
+
+    const productsLine = readLine.createInterface({
+        input: readableFile
+    });
+
+    const products: Product[] = [];
+
+    for await(let line of productsLine){
+        const productLineSplit = line.split(",");
+
+        products.push({
+            code_bar: productLineSplit[0],
+            description: productLineSplit[1],
+            price: Number(productLineSplit[2]),
+            quantity: Number(productLineSplit[3])
+        });
+
+    }
+
+    for await( let { code_bar, description, price, quantity } of products){
+        await client.products.create({
+            data: {
+                code_bar, 
+                description,
+                price,
+                quantity
+            }
+        })
+    }
+
+    return response.json(products);
+});
+
+export { router }; 
